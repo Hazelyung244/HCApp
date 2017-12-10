7513ebdbbf89658e3fac541d52802f3c4c1a803c
Yee Kay Hazel Yung <Hazelyung24@yahoo.com>
2017-12-10 15:45:45 +0000

    initial commit

---
+++ src/main/java/com/HazelCharactersMain.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@ComponentScan(basePackageClasses = HazelCharactersMain.class)
+@SpringBootApplication
+public class HazelCharactersMain {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HazelCharactersMain.class, args);
+    }
+}
